cec3e8146dbc271d2a1e0a65b392f785b98dda95
sortascary <dkaky2008@gmail.com>
2024-09-23 21:11:46 +0700

    Styling/main

--- lib/pages/menu/profile_menu.dart
+++ lib/pages/menu/profile_menu.dart
@@ -7,9 +7,50 @@ class ProfileMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(
-        () => 
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Profile'),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            SizedBox(height: 20),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Nama Pengguna',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.account_circle_sharp),
+                    title: Text('About me'),
+                    subtitle: Text('Lorem Ipsum'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.help),
+                    title: Text('Bantuan'),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              tileColor: Colors.red,
+              title: Text("Logout"),
+              onTap: () {
+                Get.toNamed('/');
+              },
+            ),
+          ],
+        ));
   }
 }
