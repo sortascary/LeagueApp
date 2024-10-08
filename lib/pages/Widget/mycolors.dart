00a589c3c49091e3d03c8a32d836cda5a1f58483
sortascary <dkaky2008@gmail.com>
2024-10-08 07:14:23 +0700

    finalized styling

--- lib/pages/Widget/mycolors.dart
+++ lib/pages/Widget/mycolors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const colorItem = Color.fromARGB(188, 139, 139, 139);
-const colorback = Color(0xFFEB5B00);
+const colorback = Colors.white;
 const colornav = Color(0xFFE4003A);
--- lib/pages/menu/home_menu.dart
+++ lib/pages/menu/home_menu.dart
@@ -9,92 +9,94 @@ class HomeMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFFEB5B00),
-        appBar: AppBar(
-          backgroundColor: colornav,
-          title: Text(
-            "Recent matches",
-            style: TextStyle(color: Colors.white),
-          ),
-          automaticallyImplyLeading: false,
-          centerTitle: true,
+      backgroundColor: Color(0xFFEB5B00),
+      appBar: AppBar(
+        backgroundColor: colornav,
+        title: Text(
+          "Home",
+          style: TextStyle(color: Colors.white),
         ),
-        body: Obx(() {
-          if (controller.isLoading.value) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          return ListView.builder(
-              itemCount: controller.matches.length,
-              itemBuilder: (context, index) {
-                final match = controller.matches[index];
-                final League = match['League'];
-                final teamH = match['HomeTeam'];
-                final teamA = match['AwayTeam'];
-                final goalH = match['HomeGoals'];
-                final goalA = match['AwayGoals'];
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+      ),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                "League Matches", // Your text above the ListView
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: controller.matches.length,
+                itemBuilder: (context, index) {
+                  final match = controller.matches[index];
+                  final League = match['League'];
+                  final teamH = match['HomeTeam'];
+                  final teamA = match['AwayTeam'];
+                  final goalH = match['HomeGoals'];
+                  final goalA = match['AwayGoals'];
 
-                return Card(
-                  color: Colors.amber,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        Text(League),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Align(
-                                alignment: Alignment.center, // Align teamH to the right
-                                child: Column(
-                                  children: [
-                                    Text(teamH),
-                                    Text(goalH)
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 8.0),
-                              child: Text('-'), 
-                            ),
-                            Expanded(
-                              child: Align(
-                                alignment:
-                                    Alignment.center, // Align teamA to the left
-                                child: Column(
-                                  children: [
-                                    Text(teamA),
-                                    Text(goalA)
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
+                  return Card(
+                    color: Colors.amber,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
                     ),
-                  ),
-                );
-              });
-        })
-
-        //Center(
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.center,
-        //   children: [
-        //     Text("Welcome to the League App!",
-        //     textAlign: TextAlign.center,
-        //     style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),),
-        //     SizedBox(height: 20,),
-        //     Icon(Icons.sports_soccer_rounded,
-        //     size: 130,),
-        //   ],
-        // ),
-        // )
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(League),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Column(
+                                    children: [
+                                      Text(teamH),
+                                      Text(goalH),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text('-'),
+                              ),
+                              Expanded(
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Column(
+                                    children: [
+                                      Text(teamA),
+                                      Text(goalA),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         );
+      }),
+    );
   }
 }
